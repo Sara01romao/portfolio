0a688515ff86c41672ca27ac6ac407a37745a2c5
Sara Romao <sara-romao@live.com>
2022-03-02 10:35:53 -0300

    animação cards

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
 
 const openMenu = document.querySelector('.open-menu');
 const menu = document.querySelector('.menu');
-const closeLine1=document.querySelector('.close-icon1');
-const closeLine2=document.querySelector('.close-icon2');
+const closeLine1 = document.querySelector('.close-icon1');
+const closeLine2 = document.querySelector('.close-icon2');
 const lineMenu = document.querySelector('.line-menu');
+const cards1 = document.querySelector('.cards-list1');
+const cards2 = document.querySelector('.cards-list2');
 
-openMenu.addEventListener('click', function(){
-   
+openMenu.addEventListener('click', function () {
+
     menu.classList.toggle('open')
     closeLine1.classList.toggle('close-x')
     closeLine2.classList.toggle('close-x')
     lineMenu.classList.toggle('lines')
 
-   
+
 })
 
 
 const navLink = document.querySelectorAll('.navLink')
 
-function linkAction(){
-   
+function linkAction() {
+
     menu.classList.toggle('open')
     closeLine1.classList.toggle('close-x')
     closeLine2.classList.toggle('close-x')
     lineMenu.classList.toggle('lines')
 
-   
+
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
@@ -34,11 +36,54 @@ const btnScroll = document.querySelector('.btnAncora');
 
 
 
-window.addEventListener("scroll", function(){
-    if(window.scrollY > 525){
+
+function animeLeft(){
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 150) {
+    
+    
+            cards1.classList.add('animeLeft');
+    
+        } else {
+    
+            cards1.classList.remove('animeLeft');
+    
+        }
+    })
+}
+
+
+animeLeft()
+
+
+function animeUp(){
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 700) {
+    
+    
+            cards2.classList.add('animeUp');
+    
+        } else {
+    
+            cards2.classList.remove('animeUp');
+    
+        }
+    })
+    
+}
+animeUp()
+
+
+
+
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 525) {
         btnScroll.classList.add('active')
-       
-    }else{
+
+
+    } else {
         btnScroll.classList.remove('active')
+
     }
 })
