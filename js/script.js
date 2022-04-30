9be17a6c017e9f75239fc0b1db6a92fdcf67fe7b
Sara Romão <sara-romao@live.com>
2022-04-30 00:21:51 -0300

    close menu

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 const menuOpen = document.querySelector("#menuOpen");
 const menu = document.querySelector(".menu");
-const socialIcons = document.querySelector('.social');
+const socialIcons = document.querySelector(".social");
+const navLink = document.querySelectorAll(".navLink");
 
 menuOpen.addEventListener("click", () => {
-    menu.classList.toggle("open");
-    
-    if(menu.classList.contains('open')){
-        console.log("open")
-        socialIcons.style.zIndex="-1"
-    }else{
-        socialIcons.style.zIndex="initial"
-    }
+  menu.classList.toggle("open");
 
+  if (menu.classList.contains("open")) {
+    console.log("open");
+    socialIcons.style.zIndex = "-1";
+  } else {
+    socialIcons.style.zIndex = "initial";
+  }
 });
+
+function linkAction() {
+  menu.classList.toggle("open");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
