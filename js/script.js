dd37a73329b0174d62266100faa6502aa5cfe106
Sara Romão <sara-romao@live.com>
2022-04-29 23:30:43 -0300

    styles alteração

--- js/script.js
+++ js/script.js
@@ -1,73 +1,15 @@
+const menuOpen = document.querySelector("#menuOpen");
+const menu = document.querySelector(".menu");
+const socialIcons = document.querySelector('.social');
 
-const openMenu = document.querySelector('.open-menu');
-const menu = document.querySelector('.menu');
-const closeLine1 = document.querySelector('.close-icon1');
-const closeLine2 = document.querySelector('.close-icon2');
-const lineMenu = document.querySelector('.line-menu');
-const cards1 = document.querySelector('.cards-list1');
-const projetos = document.querySelector('.projetos-container')
-
-openMenu.addEventListener('click', function () {
-
-    menu.classList.toggle('open')
-    closeLine1.classList.toggle('close-x')
-    closeLine2.classList.toggle('close-x')
-    lineMenu.classList.toggle('lines')
-
-
-})
-
-
-const navLink = document.querySelectorAll('.navLink')
-
-function linkAction() {
-
-    menu.classList.toggle('open')
-    closeLine1.classList.toggle('close-x')
-    closeLine2.classList.toggle('close-x')
-    lineMenu.classList.toggle('lines')
-
-
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-
-const btnScroll = document.querySelector('.btnAncora');
-
-
-
-
-function animeUp(){
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 150) {
+menuOpen.addEventListener("click", () => {
+    menu.classList.toggle("open");
     
-    
-            cards1.classList.add('animeUp');
-    
-        } else {
-    
-            cards1.classList.remove('animeUp');
-    
-        }
-    })
-}
-
-
-animeUp()
-
-
-
-
-
-
-
-window.addEventListener("scroll", function () {
-    if (window.scrollY > 525) {
-        btnScroll.classList.add('active')
-
-
-    } else {
-        btnScroll.classList.remove('active')
-
+    if(menu.classList.contains('open')){
+        console.log("open")
+        socialIcons.style.zIndex="-1"
+    }else{
+        socialIcons.style.zIndex="initial"
     }
-})
+
+});
