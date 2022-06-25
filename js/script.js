ee54af1421ecdaabe946d4a68d374602068e6680
Sara Romão <sara-romao@live.com>
2022-06-25 15:28:09 -0300

    ajustes código

--- js/script.js
+++ js/script.js
@@ -7,36 +7,13 @@ const projects = document.querySelector(".projects");
 const closeIcon = document.querySelector(".closeIcon");
 
 menuOpen.addEventListener("click", () => {
-  menu.classList.toggle("open");
-  menuOpen.classList.toggle("open");  
+    menu.classList.toggle("open");
+    menuOpen.classList.toggle("open");
 });
 
-
-
-//close menu 
+//close menu
 function linkAction() {
-  menu.classList.toggle("open");
-  menuOpen.classList.toggle("open");
+    menu.classList.toggle("open");
+    menuOpen.classList.toggle("open");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
-
-
-//anime
-/* function animeUp(){
-  window.addEventListener("scroll", function () {
-      if (window.scrollY > 650) {
-  
-  
-        projects.add('animeUp');
-  
-      } else {
-  
-        projects.remove('animeUp');
-  
-      }
-  })
-}
-
-
-animeUp()
- */
